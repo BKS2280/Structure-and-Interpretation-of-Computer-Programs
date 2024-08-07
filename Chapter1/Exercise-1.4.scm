36c65ba2bfe40fd9fe404f5a807e38a6195b8d31
BKS2280 <binaykumar.2280@gmail.com>
2024-08-07 06:48:01 +0530

    Day 4

---
+++ Chapter1/Exercise-1.4.scm
@@ -0,0 +1,11 @@
+#lang sicp
+;Exercise 1.4
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;Considering the substitution model it is easy to see that the interpreter first calls (a-plus-abs-b ..),
+;which substitutte itself with ((if ..) ..) and then replaces the result of the predicate with an operator.
+;Here the operator on a and b is itself the output of conditional. Use Dr.Racket to debug and understand.
+
+(a-plus-abs-b 3 -4)
